8ec06435f11e4bb3c616a2fff849c82780f587d9
dingding <2095801352@qq.com>
2024-09-27 16:29:14 +0800

    fix: reward reset

--- Q_Table/q_table.py
+++ Q_Table/q_table.py
@@ -4,25 +4,26 @@
 @Date    ：2024/9/23 23:05 
 @Desc    ：Q_table methods
 """
+import numpy as np
+
 from replay_buffer import ReplayBuffer
 from surrogate import Surrogate
-
+from agent import AGENT
 
 def execute():
     # -------------------------------------------------------------------------
     # parameters setup
-    replay_memory_capacity = 5000
-    training_epochs = 30000
+    replay_memory_capacity = 3000
+    num_episodes = 5000
     target = 100
     print('Repaly memory capacity: {}'.format(replay_memory_capacity))
-    print('training epochs: {}'.format(training_epochs))
+    print('training episodes: {}'.format(num_episodes))
     # -------------------------------------------------------------------------
     # prepare Gaussian Process Regression model for environment surrogate
     surrogate = Surrogate(target=target)
 
-    # -------------------------------------------------------------------------
-    # instantiate DQN agent
     replay_memory = ReplayBuffer(surrogate, capacity=replay_memory_capacity)
+    agent = AGENT()
 
 
 if __name__ == '__main__':
